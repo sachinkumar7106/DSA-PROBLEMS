dc53a5a2ff65a821b099407e962a3b6f8241be5e
Sachin Kumar <sachinkumarwork2@gmail.com>
2025-09-19 15:45:57 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (86.52%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int num(char ch){
+        if (ch == 'I'){
+            return 1;
+        }else if (ch == 'V'){
+            return 5;
+        }else if (ch == 'X'){
+            return 10;
+        }else if (ch == 'L'){
+            return 50;
+        }else if (ch == 'C'){
+            return 100;
+        }else if (ch == 'D'){
+            return 500;
+        }else if (ch == 'M'){
+            return 1000;
+        }
+        return 0;
+    }
+
+    int romanToInt(string s) {
+        int sum=0,index=0;
+        while(index<s.size()-1){
+            if(num(s[index])<num(s[index+1])){
+                sum-=num(s[index]);
+            }else{
+                sum+=num(s[index]);
+            }
+            index++;
+        }
+        sum+=num(s[index]);
+        return sum;
+    }
+}; 
