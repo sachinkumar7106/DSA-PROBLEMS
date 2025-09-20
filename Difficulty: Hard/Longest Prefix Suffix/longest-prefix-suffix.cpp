8a0cd52f8c2db72d84d3396ff1e7ebf6d716a963
Sachin Kumar <sachinkumarwork2@gmail.com>
2025-09-20 13:43:41 +0530

    Update longest-prefix-suffix.cpp

--- Difficulty: Hard/Longest Prefix Suffix/longest-prefix-suffix.cpp
+++ Difficulty: Hard/Longest Prefix Suffix/longest-prefix-suffix.cpp
@@ -5,10 +5,13 @@ class Solution {
         vector<int> lps(s.size(),0);
         int pre=0,suf=1;
         while(suf<s.size()){
+            //matched 
             if(s[pre]==s[suf]){
                 lps[suf]=pre+1;
                 pre++,suf++;
-            }else{
+            }
+            //not matched
+            else{
                 if(pre==0){
                     lps[suf]=0;
                     suf++;
