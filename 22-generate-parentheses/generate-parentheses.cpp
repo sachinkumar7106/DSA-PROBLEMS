439973878361158da00666077dd28cae8e94e434
Sachin Kumar <sachinkumarwork2@gmail.com>
2025-10-01 01:04:42 +0530

    Time: 1 ms (86.61%) | Memory: 12.9 MB (88.29%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void paren(int n,int left,int right,vector<string>&ans,string &temp){
+        if(left+right==2*n){
+            ans.push_back(temp);
+            return;
+        }
+        //left parenthesis
+        if(left<n){
+            temp.push_back('(');
+            paren(n,left+1,right,ans,temp);
+            temp.pop_back();
+        }
+        //right parenthesis
+        if(right<left){
+            temp.push_back(')');
+            paren(n,left,right+1,ans,temp);
+            temp.pop_back();
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        string temp;
+        paren(n,0,0,ans,temp);
+        return ans;
+    }
+};
