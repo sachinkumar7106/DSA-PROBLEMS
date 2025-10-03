537f6ad2452bdae3e3ea9def67ffa5191c647415
Sachin Kumar <sachinkumarwork2@gmail.com>
2025-10-03 19:44:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Tower Of Hanoi/tower-of-hanoi.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int towerOfHanoi(int n, int from, int to, int aux) {
+        // code here
+        if(n==0){
+            return 0;
+        }
+        else {
+            return pow(2,n)-1;
+        }
+    }
+};
