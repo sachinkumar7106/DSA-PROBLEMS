766c055684dff180155442cbc5c716cdec34a696
Sachin Kumar <sachinkumarwork2@gmail.com>
2025-11-17 14:58:51 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (55.31%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -1,34 +1,31 @@
 class MyStack {
 public:
-    queue <int> q1,q2;
+    queue <int> q;
     MyStack() {
         
     }
     
     void push(int x) {
-        while(!q1.empty()){
-            q2.push(q1.front());
-            q1.pop();
-        }
-        q1.push(x);
-        while(!q2.empty()){
-            q1.push(q2.front());
-            q2.pop();
+        q.push(x);
+        int n=q.size();
+        for (int i=1;i<n;i++){
+            q.push(q.front());
+            q.pop();
         }
     }
     
     int pop() {
-        int ans=q1.front();
-        q1.pop();
+        int ans=q.front();
+        q.pop();
         return ans;
     }
     
     int top() {
-        return q1.front();
+        return q.front();
     }
     
     bool empty() {
-        return q1.empty();
+        return q.empty();
     }
 };
 
