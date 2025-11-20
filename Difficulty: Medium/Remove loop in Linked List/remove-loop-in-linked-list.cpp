f6d0bd02ff4c500415436e96e309cf379aef6dab
Sachin Kumar <sachinkumarwork2@gmail.com>
2025-11-20 19:32:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Remove loop in Linked List/remove-loop-in-linked-list.cpp
+++ Difficulty: Medium/Remove loop in Linked List/remove-loop-in-linked-list.cpp
@@ -25,11 +25,25 @@ class Solution {
         if(fast==NULL||fast->next==NULL){
             return;
         }
-        slow=head;
+        // length of the loop 
+        slow=fast->next;
+        int count=1;
+        while(slow!=fast){
+            count++;
+            slow=slow->next;
+        }
+        
+        // nodes inside the loop
+        slow =head,fast=head;
+        // fast ko count distance dur rakhenge 
+        while(count--){
+            fast=fast->next;
+        }
         while(slow!=fast){
             slow=slow->next;
             fast=fast->next;
         }
+        //bring slow to last node
         while(slow->next!=fast){
             slow=slow->next;
         }
