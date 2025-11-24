26c10d6995d8eabfff69b1b91a733c75f180ab6a
Sachin Kumar <sachinkumarwork2@gmail.com>
2025-11-24 12:43:24 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (54.10%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -7,8 +7,8 @@ public:
             if (arr1[s[i]] != arr2[t[i]]) {
                 return false;
             }
-            arr1[s[i]] = i + 1;
-            arr2[t[i]] = i + 1;
+            arr1[s[i]] = i+1;
+            arr2[t[i]] = i+1;
         }
         return true;
     }
