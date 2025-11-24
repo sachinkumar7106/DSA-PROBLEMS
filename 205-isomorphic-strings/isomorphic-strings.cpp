9956987e27ef9d9b502f7ac4c79161b3b79f1713
Sachin Kumar <sachinkumarwork2@gmail.com>
2025-11-24 12:42:17 +0530

    Time: 4 ms (23.27%) | Memory: 9.3 MB (78.53%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -3,7 +3,7 @@ public:
     bool isIsomorphic(string s, string t) {
         int arr1[256] = {0}, arr2[256] = {0};
 
-        for (int i = 0; i < s.size(); ++i) {
+        for (int i = 0; i < s.size(); i++) {
             if (arr1[s[i]] != arr2[t[i]]) {
                 return false;
             }
