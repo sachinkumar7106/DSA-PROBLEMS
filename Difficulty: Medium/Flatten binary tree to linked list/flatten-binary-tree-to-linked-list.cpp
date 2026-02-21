8f67a5f38b7801870e0ae771e953106631b6a62b
Sachin Kumar <sachinkumarwork2@gmail.com>
2026-02-21 23:28:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Flatten binary tree to linked list/flatten-binary-tree-to-linked-list.cpp
@@ -0,0 +1,22 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    void flatten(Node *root) {
+        // code here
+        while(root){
+            if(!root->left){
+                root=root->right;
+            }else{
+                Node*curr=root->left;
+                while(curr->right){
+                    curr=curr->right;
+                }
+                curr->right=root->right;
+                root->right=root->left;
+                root->left=NULL;
+                root=root->right;
+            }
+        }
+    }
+};
