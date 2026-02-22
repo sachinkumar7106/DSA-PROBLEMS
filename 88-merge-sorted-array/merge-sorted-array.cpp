7e9eb51127760b805382bc5d3e969fc05d193815
Sachin Kumar <sachinkumarwork2@gmail.com>
2026-02-22 14:49:03 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (40.75%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -4,7 +4,7 @@ public:
         int k=m+n-1;
         int i=m-1,j=n-1;
         while(i>=0&&j>=0){
-            if(nums1[i]>nums2[j]){
+            if(nums1[i]>=nums2[j]){
                 nums1[k--]=nums1[i];
                 i--;
             }else{
