1e0c02f151e0956d2fad506d057ff98f6bf90c0d
Sachin Kumar <sachinkumarwork2@gmail.com>
2026-06-06 11:42:47 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (62.52%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -2,18 +2,38 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n=nums.size();
-        vector<int>res;
-        unordered_map<int,int>freq;
+        int cnt1=0,ele1;
+        int cnt2=0,ele2;
         for(int i=0;i<n;i++){
-            freq[nums[i]]++;
-            if(res.empty()||res[0]!=nums[i]){
-                if(freq[nums[i]]>n/3){
-                    res.push_back(nums[i]);
-                }
-                if(res.size()==2){
-                    return res;
-                }
+            if(cnt1==0&&ele2!=nums[i]){
+                cnt1++;
+                ele1=nums[i];
+            }else if(cnt2==0&&ele1!=nums[i]){
+                cnt2++;
+                ele2=nums[i];
+            }else if(ele1==nums[i]){
+                cnt1++;
+            }else if(ele2==nums[i]){
+                cnt2++;
+            }else{
+                cnt1--;cnt2--;
             }
+        }
+        vector<int>res;
+        int count1=0,count2=0;
+        for(int i=0;i<n;i++){
+            if(ele1==nums[i]){
+                count1++;
+            }
+            if(ele2==nums[i]){
+                count2++;
+            }
+        }
+        if(count1>n/3){
+            res.push_back(ele1);
+        }
+        if(count2>n/3){
+            res.push_back(ele2);
         }
         return res;
     }
