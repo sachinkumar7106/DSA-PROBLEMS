8b2dc5baa5e50898adc01ee48d63462d306ee23a
Sachin Kumar <sachinkumarwork2@gmail.com>
2026-06-23 12:32:01 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (40.71%) - LeetSync

---
+++ 214-shortest-palindrome/shortest-palindrome.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string shortestPalindrome(string s) {
+        string rev=s;
+        reverse(rev.begin(),rev.end());
+        string temp=s+"#"+rev;
+        int n=temp.size();
+        vector<int> lps(n, 0);
+
+        int len = 0;
+        int i = 1;
+
+        while (i < n) {
+            if (temp[i] == temp[len]) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+
+        string ns=s.substr(lps[n-1]);
+        reverse(ns.begin(),ns.end());
+        return ns+s;
+    }
+};
