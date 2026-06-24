5218e23146dc438852532b9d0d073f83dbb2fd5c
Sachin Kumar <sachinkumarwork2@gmail.com>
2026-06-24 18:53:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Zero Sum Subarrays/zero-sum-subarrays.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    int findSubarray(vector<int> &arr) {
+        // code here.
+        int n=arr.size();
+        unordered_map<int,int>mp;
+        mp[0]++;
+        int sum=0,count=0;
+
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+            if(mp.find(sum)!=mp.end()){
+                count+=mp[sum]++;
+            }else{
+                mp[sum]++;
+            }
+        }
+        return count;
+    }
+};
