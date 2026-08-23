daf5c9c1cac7c3ff3051337b03e0a94b8e45a396
Sachin Kumar <sachinkumarwork2@gmail.com>
2026-08-24 00:59:34 +0530

    Time: 0 ms (100.00%) | Memory: 21 MB (25.54%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,25 +1,50 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int n=matrix.size(),m=matrix[0].size();
-        vector<bool>row(n,false);
-        vector<bool>col(m,false);
+        int n=matrix.size();
+        int m=matrix[0].size();
+
+        bool frz=false;
+        bool fcz=false;
+
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(matrix[i][j]==0){
-                    if(!row[i])
-                    row[i]=true;
-                    if(!col[j])
-                    col[j]=true;
-                }
+            if(matrix[i][0]==0){
+                frz=true;
+                break;
+            }
+        }
+
+        for(int i=0;i<m;i++){
+            if(matrix[0][i]==0){
+                fcz=true;
+                break;
             }
         }
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(row[i]||col[j]){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
+            }
+        }
+
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(!matrix[i][0]||!matrix[0][j]){
                     matrix[i][j]=0;
                 }
+            }
+        }
+        if(frz){
+            for(int i=0;i<n;i++){
+                matrix[i][0]=0;
+            }
+        }
+        if(fcz){
+            for(int i=0;i<m;i++){
+                matrix[0][i]=0;
             }
         }
     }
