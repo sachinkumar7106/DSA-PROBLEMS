f2fe452bad9cb363e1515b9b7834678293eb02ec
Sachin Kumar <sachinkumarwork2@gmail.com>
2026-08-24 01:00:32 +0530

    Time: 0 ms (100.00%) | Memory: 21 MB (42.41%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -21,8 +21,8 @@ public:
             }
         }
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
                 if(matrix[i][j]==0){
                     matrix[i][0]=0;
                     matrix[0][j]=0;
